f7993bc5e8265feb996c65cad2d149e181634271
Eesha <eesha.barua@gmail.com>
2021-09-29 23:09:24 -0500

    Added Photo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import '../css/Home.css';
 
 // Add image file imports here
 import yousef from '../utils/images/yousef_ahmed.jpg';
+import eesha from '../utils/images/eesha_barua.jpg';
 
 function Home() {
   const people = [
@@ -21,7 +22,7 @@ function Home() {
       {
         name: 'Eesha',
         link: '/eesha',
-        image: yousef,
+        image: eesha,
       },
       {
         name: 'Ellie',
