97b40516bb6e2bd9f946d0bb728e2d3ab974f941
Grace Zhang <gracewzhang01@gmail.com>
2021-10-06 18:05:11 -0500

    oval banner

--- client/src/pages/Grace.jsx
+++ client/src/pages/Grace.jsx
@@ -1,14 +1,19 @@
-// @flow
+// @flow 
 import React from 'react';
 
-import '../css/Home.css';
+import '../css/Grace.css';
 
 function Grace() {
   return (
     <>
-      <h1>Grace's Page</h1>
+      <div className = "banner">
+        <img src="https://images8.alphacoders.com/685/685929.png" alt="Mushishi Wallpaper" width="100%" height="400px"/>       
+        <div className = "header"> ~ Grace Zhang ~ </div>
+        <div className = "intro"> Hello! I'm Grace, a freshman in CS. </div>
+      </div>
+
     </>
   );
 }
-
 export default Grace;
+
