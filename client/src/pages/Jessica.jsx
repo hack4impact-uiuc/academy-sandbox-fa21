2b59dadbf7678cef09ad6bded174667883631a9e
Jessica Singh <jessicasingh@outlook.com>
2021-10-07 21:32:06 -0500

    Format using Prettier

--- client/src/pages/Jessica.jsx
+++ client/src/pages/Jessica.jsx
@@ -8,12 +8,11 @@ function Jessica() {
   return (
     <>
       <h1>Jessica's Page</h1>
-        <h1>Freshman in CS</h1>
-      
+      <h1>Freshman in CS</h1>
+
       <p>Hi! Some things about me: I like to sing, dance, sketch, and eat.</p>
       <p>I'm from Seattle, WA</p>
-      <img src={bg} alt="Mount Rainier" height = "1000"></img>
-      
+      <img src={bg} alt="Mount Rainier" height="1000"></img>
     </>
   );
 }
