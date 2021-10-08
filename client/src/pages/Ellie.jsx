f6d0a604756461b247e82fa32638acc1f914d9f2
unknown <epopo2@illinois.edu>
2021-10-08 13:59:32 -0500

    Create styling for Ellie's page

--- client/src/pages/Ellie.jsx
+++ client/src/pages/Ellie.jsx
@@ -2,12 +2,30 @@
 import React from 'react';
 
 import '../css/Ellie.css';
+import Img1 from '../utils/images/estrella2_popoca.jpg';
+
 
 function Ellie() {
   return (
     <>
-      <h1>Ellie's Page</h1>
-    </>
+    <head> 
+    <title>i'm ellie</title>
+    
+  </head>
+    <body>
+      <div className="container">
+
+        <div className="picture">
+          
+        <img id="home-picture" src={Img1} alt="" />
+        
+        <div className="text">
+          <h2>hello! My name is Estrella Popoca!</h2> 
+          </div>
+        </div>
+      </div>
+      </body>
+</>
   );
 }
 
