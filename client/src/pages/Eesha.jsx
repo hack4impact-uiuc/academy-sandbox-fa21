818445703180625640a2e09c8d4a28f9fc34563f
Eesha <eesha.barua@gmail.com>
2021-10-07 21:03:55 -0500

    Add Personal Website Eesha

--- client/src/pages/Eesha.jsx
+++ client/src/pages/Eesha.jsx
@@ -1,14 +1,78 @@
 // @flow
 import React from 'react';
 
-import '../css/Home.css';
+import '../css/Eesha.css';
 
 function Eesha() {
   return (
     <>
       <h1>Eesha's Page</h1>
+      <h1>WHY ZUTARA WAS ROBBED</h1>
+          <h3>Explained congress-prep style by Eesha Barua and other parties.</h3>
+        <h2>Framework: Thematic Continuity in ATLA</h2>
+            <h4> <i> From a thematic, narrative standpoint it makes more sense for a person with Katara’s character to 
+              end up with a person like Zuko. It reaffirms the crucial fabric of ATLA’s story that binds together many 
+              other plot points of the series, and contradicting this central theme in ATLA diminishes the entire show’s 
+              impact and consistency. </i> </h4>
+              <h5>Value Criterion: I take BALANCE as the highest value of a relationship for the following reasons. </h5>
+        <h2> <b> CONTENTION 1: The theme of BALANCE most definitely drives the creation, development, and resolutions of ATLA.</b> </h2>
+          <h3>This show is full of commentary on spirituality, destiny and balance in the world. </h3>
+        <h2>Subpoint A : <i>Exposition / Balance. </i>The purpose of the Avatar is to restore balance to the world.</h2>
+          <h3>The Avatar is uniquely powerful to single handedly restore balance to the world because of their ability 
+            to bend all four elements.</h3>
+            <h4>The exposition of ATLA and the premise for this story to even exist is precedented on the intense pursuit 
+              of balance in the Avatar Universe, thus it must be the lens through which we examine character development 
+              and what is most beneficial to a character’s personal growth and self-actualization.</h4>
+        <h2>Subpoint 2: <i> Development / Spirituality. </i> Daoist philosophy of balance has resolved mid-story and character 
+                arcs in Avatar.</h2>
+          <h3>The Siege of the North Arc </h3>
+            <h4>This arc is the most obvious in its reliance on Daoist philosophy to restore balance. The Fire Nation 
+              became stronger after Commander Zhao killed Tui, the moon spirit. It was only subdued after La assumed Aang’s 
+              body in the avatar state and the spirit only ceased its destruction and rampage after Yue brought life back 
+              into Tui. The thematic element of Yin and Yang is heavily used to add tension to and resolve this arc.</h4>
+              <h5> <u> Koh //  The Siege of the North, Part 2 // Book 1 </u> </h5>
+              <h5>“You've already met them, actually. Tui and La, your moon and ocean, have always circled each other in an 
+                eternal dance. They balance each other. Push and Pull. Life and Death. Good and Evil. Yin and Yang.”</h5>
+
+              <img src="../utils/images/tui_and_la.png" alt="tui and la" />
+              <h5>This imagery is pretty vocal in the implications of romance between the two spirits. Furthermore, the 
+                Daoist philosophy of Yin and Yang strongly implies that balance ideally exists between two beings that 
+                complement each other. The Avatar is the SINGULAR exception to this norm, as the Avatar is <b> expected to 
+                single handedly achieve balance. </b> ( Which is important to understand later ) </h5>
+          <h3>The Cave of Two Lovers</h3>
+              <h5><u>Secret Tunnel!!! guy // The Cave of Two Lovers // Book 2</u></h5>
+              <h5>"Two lovers, forbidden from one another</h5>
+              <h5>A war divides their people</h5> 
+              <h5>And a mountain divides them apart</h5> 
+              <h5>Built a path to be together..."</h5>
+              <img src="../utils/images/cave_of_two_lovers.png" alt= "cave of two lovers" />
+              <h5>Again, the imagery here quite directly signifies that romance blooms in beings from opposite worlds but 
+                parallel characteristics, another nod to Yin and Yang.</h5>
+          <h3>The Great Divide</h3>
+              <h5>The differences between the Gan Jin and the Zhang tribe are disparate, but surprisingly parallel. 
+                They both trace back to the same origin story. </h5>
+                <h6>Almost too noticeably, one tribe ends in -in, Gan Jins, and the other tribe ends in -ang, the Zhang. 
+                  This is another spiritual nod to the nature of village names in Taoist mainland China (the influence 
+                  for the Earth Kingdom in the Avatar Universe, as the Earth Kingdom enompasses most of Asia).</h6>
+              <h5><u>John Bellaimey // the Hidden Meanings of Yin and Yang, Ted-Ed // Aug 2013</u></h5>
+              <h5>Villages on the sunny side of a valley in China have names like Liuyang or Shiyang, but on the shady side, 
+                for example, of the Yangtze River Valley, there’s Jiangyin.</h5>
+            <h4>Let’s examine the central conflict in this episode.</h4>
+              <h5> <u>The Great Divide //  Avatar Wiki // 2005 </u> </h5>
+                <h6>The Gan Jins tell Katara of their tribal forefather, an earthbender named Jin Wei, who was tasked with the job 
+                  of taking the sacred crystal orb from a gate in the east to a gate in the west as part of a redemption ritual. 
+                  They claim that, as he neared the western gate, Jin Wei was assaulted by a thief, a Zhang named Wei Jin, who 
+                  stole the orb out of greed. The Zhangs, on the other hand, tell Sokka that their ancestor, Wei Jin, found Jin Wei 
+                  passed out on the ground near the western gate. Wei Jin stopped to help the man and was told about the redemption 
+                  ritual and the orb's importance. Rejecting assistance, Jin Wei asked Wei Jin to take the orb back to his tribe, 
+                  which Wei Jin did. Upon arriving at Gan Jin's village, however, Jin Wei's tribe arrested him for stealing the orb and 
+                  imprisoned him for twenty years. Both sides claim that they will never forget the terrible injustice dealt to them by the other.</h6>
+              <img src="../utils/images/weijin_and_jinwei.png" alt= "weijin and jinwei" />
+            <h4>This episode is resolved with an (albeit phony) understanding that Wei Jin and Jin Wei had a special relationship as brothers 
+              and coexisted peacefully. This nod to balance reaffirms the sheer amount of story-building power in the theme of balance in ATLA.</h4>
+          <h3>Uncle Iroh’s Waterbending Move</h3>
     </>
-  );
+  );  
 }
 
 export default Eesha;
