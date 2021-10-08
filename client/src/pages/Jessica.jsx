0d2e9a3ec26caedde59273a31de011666728cbd3
Jessica Singh <jessicasingh@outlook.com>
2021-10-07 20:56:12 -0500

    Add text and images

--- client/src/pages/Jessica.jsx
+++ client/src/pages/Jessica.jsx
@@ -1,12 +1,19 @@
 // @flow
 import React from 'react';
 
-import '../css/Home.css';
+import '../css/Jessica.css';
+import bg from '../utils/images/bg.jpg';
 
 function Jessica() {
   return (
     <>
       <h1>Jessica's Page</h1>
+        <h1>Freshman in CS</h1>
+      
+      <p>Hi! Some things about me: I like to sing, dance, sketch, and eat.</p>
+      <p>I'm from Seattle, WA</p>
+      <img src={bg} alt="Mount Rainier" height = "1000"></img>
+      
     </>
   );
 }
