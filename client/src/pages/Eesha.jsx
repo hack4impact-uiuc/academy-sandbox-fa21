d1d23485784122d4ca880e220cce71899105ddf8
Eesha <eesha.barua@gmail.com>
2021-10-20 12:06:33 -0500

    Update Personal Website Eesha

--- client/src/pages/Eesha.jsx
+++ client/src/pages/Eesha.jsx
@@ -2,106 +2,180 @@
 import React from 'react';
 
 import '../css/Eesha.css';
-import cave from '../utils/images/cave_of_two_lovers.png';
-import la from '../utils/images/tui_and_la.png';
-import weijin from '../utils/images/weijin_and_jinwei.png';
-import sunset from '../utils/images/sunset.jpg'
+import appa from '../utils/images/eesha-img/appa-flying.gif';
+import sun from '../utils/images/eesha-img/sun.svg';
+import asc from '../utils/images/eesha-img/ascendent.svg';
+import moon from '../utils/images/eesha-img/moon.svg';
+import mer from '../utils/images/eesha-img/mercury.svg';
+import ven from '../utils/images/eesha-img/venus.svg';
+import mar from '../utils/images/eesha-img/mars.svg';
+import jup from '../utils/images/eesha-img/jupiter.svg';
+import sat from '../utils/images/eesha-img/saturn.svg';
+import ur from '../utils/images/eesha-img/uranus.svg';
+//import strength from '../utils/images/eesha-img/strength.jpg';
+//import devil from '../utils/images/eesha-img/devil.jpg';
+import eightwands from '../utils/images/eesha-img/8wands.jpg';
+import chariot from '../utils/images/eesha-img/chariot.jpg';
+import fourswords from '../utils/images/eesha-img/4swords.jpg';
+import sixwands from '../utils/images/eesha-img/6wands.jpg';
+import fourcups from '../utils/images/eesha-img/4cups.jpg';
+import fiveswords from '../utils/images/eesha-img/5swords.jpg';
+import blocked from '../utils/images/eesha-img/blocked.png';
+import lovers from '../utils/images/eesha-img/lovers.jpg';
+import tower from '../utils/images/eesha-img/tower.jpg';
 
 function Eesha() {
   return (
     <>
-    <div className="hero-image">
-      <div className="hero-text">
-        <h1>Eesha's Page</h1>
-        <p>WHY ZUTARA WAS ROBBED</p>
+      <div className="banner">
+        <div>
+          <h1 className="eesha-title"> eesha meets world.</h1>
+          <h4 className="eesha-text"> eesha barua || academy member || fall 2021</h4>
+          <h3 className="eesha-text"> <i> ~ blue skies in, gray skies out ~ </i> </h3>
+        </div>  
       </div>
-    </div>
-    <img 
-      src={sunset}
-      alt="sunset"
-    ></img>
-      <h1>Eesha's Page</h1>
-      <h1>WHY ZUTARA WAS ROBBED</h1>
-          <h3>Explained congress-prep style by Eesha Barua and other parties.</h3>
-        <h2>Framework: Thematic Continuity in ATLA</h2>
-            <h4> <i> From a thematic, narrative standpoint it makes more sense for a person with Katara’s character to 
-              end up with a person like Zuko. It reaffirms the crucial fabric of ATLA’s story that binds together many 
-              other plot points of the series, and contradicting this central theme in ATLA diminishes the entire show’s 
-              impact and consistency. </i> </h4>
-              <h5>Value Criterion: I take BALANCE as the highest value of a relationship for the following reasons. </h5>
-        <h2> <b> CONTENTION 1: The theme of BALANCE most definitely drives the creation, development, and resolutions of ATLA.</b> </h2>
-          <h3>This show is full of commentary on spirituality, destiny and balance in the world. </h3>
-        <h2>Subpoint A : <i>Exposition / Balance. </i>The purpose of the Avatar is to restore balance to the world.</h2>
-          <h3>The Avatar is uniquely powerful to single handedly restore balance to the world because of their ability 
-            to bend all four elements.</h3>
-            <h4>The exposition of ATLA and the premise for this story to even exist is precedented on the intense pursuit 
-              of balance in the Avatar Universe, thus it must be the lens through which we examine character development 
-              and what is most beneficial to a character’s personal growth and self-actualization.</h4>
-        <h2>Subpoint 2: <i> Development / Spirituality. </i> Daoist philosophy of balance has resolved mid-story and character 
-                arcs in Avatar.</h2>
-          <h3>The Siege of the North Arc </h3>
-            <h4>This arc is the most obvious in its reliance on Daoist philosophy to restore balance. The Fire Nation 
-              became stronger after Commander Zhao killed Tui, the moon spirit. It was only subdued after La assumed Aang’s 
-              body in the avatar state and the spirit only ceased its destruction and rampage after Yue brought life back 
-              into Tui. The thematic element of Yin and Yang is heavily used to add tension to and resolve this arc.</h4>
-              <h5> <u> Koh //  The Siege of the North, Part 2 // Book 1 </u> </h5>
-              <h5>“You've already met them, actually. Tui and La, your moon and ocean, have always circled each other in an 
-                eternal dance. They balance each other. Push and Pull. Life and Death. Good and Evil. Yin and Yang.”</h5>
+      
+      <img src={appa} alt="appa flying" className="icon"></img>
 
-                <center><img
-                    alt={la}
-                    src={la}
-                    width="600"
-                    height="600"
-                  /></center>
-              <h5>This imagery is pretty vocal in the implications of romance between the two spirits. Furthermore, the 
-                Daoist philosophy of Yin and Yang strongly implies that balance ideally exists between two beings that 
-                complement each other. The Avatar is the SINGULAR exception to this norm, as the Avatar is <b> expected to 
-                single handedly achieve balance. </b> ( Which is important to understand later ) </h5>
-          <h3>The Cave of Two Lovers</h3>
-              <h5><u>Secret Tunnel!!! guy // The Cave of Two Lovers // Book 2</u></h5>
-              <h5>"Two lovers, forbidden from one another</h5>
-              <h5>A war divides their people</h5> 
-              <h5>And a mountain divides them apart</h5> 
-              <h5>Built a path to be together..."</h5>
-              <center><img
-                    alt={cave}
-                    src={cave}
-                    width="600"
-                    height="600"
-                  /></center>
-              <h5>Again, the imagery here quite directly signifies that romance blooms in beings from opposite worlds but 
-                parallel characteristics, another nod to Yin and Yang.</h5>
-          <h3>The Great Divide</h3>
-              <h5>The differences between the Gan Jin and the Zhang tribe are disparate, but surprisingly parallel. 
-                They both trace back to the same origin story. </h5>
-                <h6>Almost too noticeably, one tribe ends in -in, Gan Jins, and the other tribe ends in -ang, the Zhang. 
-                  This is another spiritual nod to the nature of village names in Taoist mainland China (the influence 
-                  for the Earth Kingdom in the Avatar Universe, as the Earth Kingdom enompasses most of Asia).</h6>
-              <h5><u>John Bellaimey // the Hidden Meanings of Yin and Yang, Ted-Ed // Aug 2013</u></h5>
-              <h5>Villages on the sunny side of a valley in China have names like Liuyang or Shiyang, but on the shady side, 
-                for example, of the Yangtze River Valley, there’s Jiangyin.</h5>
-            <h4>Let’s examine the central conflict in this episode.</h4>
-              <h5> <u>The Great Divide //  Avatar Wiki // 2005 </u> </h5>
-                <h6>The Gan Jins tell Katara of their tribal forefather, an earthbender named Jin Wei, who was tasked with the job 
-                  of taking the sacred crystal orb from a gate in the east to a gate in the west as part of a redemption ritual. 
-                  They claim that, as he neared the western gate, Jin Wei was assaulted by a thief, a Zhang named Wei Jin, who 
-                  stole the orb out of greed. The Zhangs, on the other hand, tell Sokka that their ancestor, Wei Jin, found Jin Wei 
-                  passed out on the ground near the western gate. Wei Jin stopped to help the man and was told about the redemption 
-                  ritual and the orb's importance. Rejecting assistance, Jin Wei asked Wei Jin to take the orb back to his tribe, 
-                  which Wei Jin did. Upon arriving at Gan Jin's village, however, Jin Wei's tribe arrested him for stealing the orb and 
-                  imprisoned him for twenty years. Both sides claim that they will never forget the terrible injustice dealt to them by the other.</h6>
-                  <center><img
-                    alt={weijin}
-                    src={weijin}
-                    width="600"
-                    height="600"
-                  /></center>
-            <h4>This episode is resolved with an (albeit phony) understanding that Wei Jin and Jin Wei had a special relationship as brothers 
-              and coexisted peacefully. This nod to balance reaffirms the sheer amount of story-building power in the theme of balance in ATLA.</h4>
-          <h3>Uncle Iroh’s Waterbending Move</h3>
+      <div className="callout">
+        <div className="callout-header">my star chart</div>
+        <div className="callout-container">
+          <p className="eesha-text-unpadded">i was born at 11.08 pm, december 15, 2002, which means: </p>
+        </div>
+      </div>
+
+      <div className="flex-grid-container">
+        <div className="grid-one">
+          <img src={sun} alt="sun-sign" />
+          <p className="eesha-sign-chart-h1">sun || sagittarius || 24°1'37"</p>
+          <p className="eesha-sign-chart-h2">i have strong ideas about meaning and what's right or wrong.</p>
+          <p className="eesha-sign-chart-h2">i live to be free, and to learn.</p>
+        </div>
+
+        <div className="grid-two">
+          <img src={asc} alt="asc-sign" />
+          <p className="eesha-sign-chart-h1">ascendent || virgo || 11°45'54"</p>
+          <p className="eesha-sign-chart-h2">i am precise, diligent, peace-seeking, and organized.</p>
+          <p className="eesha-sign-chart-h2">i may seem a bit too attached to work, details, and the pursuit of perfection.</p>
+        </div>
+
+        <div className="grid-three">
+          <img src={moon} alt="moon-sign" />
+          <p className="eesha-sign-chart-h1">moon || taurus || 13°6'26"</p>
+          <p className="eesha-sign-chart-h2">i am deeply loyal to the people i care about.</p>
+          <p className="eesha-sign-chart-h2">i often feel powerless to fix messy situations.</p>
+        </div>
+
+        <div className="grid-four">
+          <img src={mer} alt="mer-sign" />
+          <p className="eesha-sign-chart-h1">mercury || capricorn || 10°59'53"</p>
+          <p className="eesha-sign-chart-h2">i tend to be a poor communicator (oops).</p>
+          <p className="eesha-sign-chart-h2">i am curious about and inclined to analyze my love life, happiness, and creative projects.</p>
+        </div>
+
+        <div className="grid-five">
+          <img src={ven} alt="ven-sign" />
+          <p className="eesha-sign-chart-h1">venus || scorpio || 10°20'31"</p>
+          <p className="eesha-sign-chart-h2">my romantic side is mysterious, passionate, and seductive..</p>
+          <p className="eesha-sign-chart-h2">aaaand that's enough of that.</p>
+        </div>
+
+        <div className="grid-six">
+          <img src={mar} alt="mar-sign" />
+          <p className="eesha-sign-chart-h1">mars || scorpio || 9°22'46"</p>
+          <p className="eesha-sign-chart-h2">i assert myself in a way that is serious and incisive.</p>
+          <p className="eesha-sign-chart-h2">once i decide i want to do something, i don't hold back.</p>
+        </div>
+
+        <div className="grid-seven">
+          <img src={jup} alt="jup-sign" />
+          <p className="eesha-sign-chart-h1">jupiter || leo || 17°52'53"</p>
+          <p className="eesha-sign-chart-h2">i grow and find understanding through magnanimity, inspiring confidence, thinking big, and feeling good about myself.</p>
+        </div>
+
+        <div className="grid-eight">
+          <img src={sat} alt="sat-sign" />
+          <p className="eesha-sign-chart-h1">saturn || gemini || 25°43'34"</p>
+          <p className="eesha-sign-chart-h2">i struggle with a lack of focus, restless energy, and getting bored easily.</p>
+          <p className="eesha-sign-chart-h2">no i don't.</p>
+        </div>
+
+        <div className="grid-nine">
+          <img src={ur} alt="ur-sign" />
+          <p className="eesha-sign-chart-h1">uranus || aquarius || 25°38'59"</p>
+          <p className="eesha-sign-chart-h2">other generations are shocked by our generation's unconventionality, intellectuality, and detachedness.</p>
+          <p className="eesha-sign-chart-h2">is this true, h4i?</p>
+        </div>
+      </div>
+
+      <div className="callout">
+        <div className="callout-header">my celtic spread</div>
+        <div className="callout-container">
+          <p className="eesha-text-unpadded">from my latest reading </p>
+        </div>
+      </div>
+
+      <div className="flex-grid-container-cards">
+        <div className="grid-4">
+          <img src={eightwands} alt="8 of wands" />
+        </div>
+
+        <div className="grid-6">
+          <img src={chariot} alt="the chariot" />
+        </div>
+
+        <div className="grid-8">
+          <img src={fourswords} alt="4 of swords" />
+        </div>
+
+        <div className="grid-9">
+          <img src={sixwands} alt="6 of wands" />
+        </div>
+
+        <div className="grid-10">
+          <img src={blocked} alt="blocked" />
+        </div>
+
+        <div className="grid-11">
+          <img src={fourcups} alt="4 of cups" />
+        </div>
+
+        <div className="grid-12">
+          <img src={fiveswords} alt="5 of swords" />
+        </div>
+
+        <div className="grid-14">
+          <img src={lovers} alt="lovers" />
+        </div>
+
+        <div className="grid-16">
+          <img src={tower} alt="tower" />
+        </div>
+      </div>
+
+      <div className="callout">
+        <div className="callout-header">my MBTI</div>
+        <div className="callout-container">
+          <p className="eesha-text-unpadded">i am a protagonist (ENFJ) ! which means: </p>
+        </div>
+      </div>
+
+      <div className="callout">
+        <div className="callout-header">my hogwarts house</div>
+        <div className="callout-container">
+          <p className="eesha-text-unpadded">i am a: ravenclaw with a beagle patronus and a ___ wand: </p>
+        </div>
+      </div>
+
+      <div className="callout">
+        <div className="callout-header">my Pottermore</div>
+        <div className="callout-container">
+          <p className="eesha-text-unpadded">i am a: ravenclaw with a beagle patronus and a ___ wand: </p>
+        </div>
+      </div>
+
     </>
-  );  
+  );
 }
 
 export default Eesha;
