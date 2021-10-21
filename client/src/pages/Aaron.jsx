9065fcc1ff1912d2cc9ddaf8a680fd6637a8fc54
Aaron Alexander <a.alexander72003@gmail.com>
2021-10-21 15:11:27 -0500

    Final changes to personal website

--- client/src/pages/Aaron.jsx
+++ client/src/pages/Aaron.jsx
@@ -7,38 +7,24 @@ function Aaron() {
   return (
     <>
       <div className="animated">
-        <h1 className="main-heading">Aaron Alexander</h1>
-        <h2 className="sub-heading">
-          hack4impact academy member and future software developer
-        </h2>
-        <div className="navBar">
-          <h2>
-            <span className="links">bio</span> |{' '}
-            <span className="links">resume</span> |{' '}
-            <span className="links">projects</span>
-          </h2>
-        </div>
-        <img
-          src="https://i.ibb.co/XS1m7Fc/aaron-thinking.png"
-          alt="aaron-thinking"
-          className="intenseThought"
-        />
-        <p className="innards">
-          Hello world! My name is Aaron Alexander, and this is my website.
-          There'll be more content in the future, but for now this is the best
-          that I can do. Hooray!
-        </p>
-        <p className="innards">
-          Favorite song right now: "Under the Bridge" by Red Hot Chili Peppers
-        </p>
-        <p className="innards">
-          Favorite food right now: Chicken dumpling soup at the Ike.
-          Best-tasting food thus far
-        </p>
-        <p className="innards">
-          Favorite album: Axis Bold as Love by Jimi Hendrix, the only album I
-          have on vinyl thus far
-        </p>
+        <div className="main-header">
+          <img
+          src="https://i.imgur.com/GyqsEtg.jpg?1"
+          alt="Me!"
+          className="header-image"
+          />
+       <h1 className="name">Aaron Alexander</h1>
+       <h2 className="subtext">h4i academy member | future software developer | uiuc student</h2>
+       <hr className="cool-hr"></hr>
+       <ul className="nav">
+       <li className="navbar"><a href="https://www.linkedin.com/in/aaron-alexander-735328198/" className="cool-link">linkedin</a></li> <span className="button">•</span>
+       <li className="navbar"><a href="https://github.com/AaronAlexander-debug" className="cool-link">github</a></li> <span className="button">•</span>
+       <li className="navbar"><a href="https://docs.google.com/document/d/1lEKDekVVReZomR9c-3moQASVCpmsuOkb3UQ1ziedIDU/edit?usp=sharing" className="cool-link">resume</a></li> <span className="button">•</span>
+       <li className="navbar"><a href="https://www.instagram.com/thereal.aaronalexander/" className="cool-link">instagram</a></li>
+       </ul>
+       </div>
+       <hr className="cool-hr"></hr>
+       <p className="cool-intro">Welcome to my personal website! I am Aaron Alexander, a freshman studying computer science at the University of Illinois at Urbana-Champaign and a proud Academy Member at Hack4Impact. So so so excited to start my journey in software development at such a great school!</p>
       </div>
     </>
   );
