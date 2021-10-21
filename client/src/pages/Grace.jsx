eab12e6dea32561cea151e20d37a0c0b27a14c4c
Grace Zhang <gracewzhang01@gmail.com>
2021-10-21 13:26:08 -0500

    format code

--- client/src/pages/Grace.jsx
+++ client/src/pages/Grace.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 import '../css/Grace.css';
-import GraceVideo from '../pages/GraceVideo.jsx'
+import GraceVideo from '../pages/GraceVideo.jsx';
 
 function Grace() {
   return (
@@ -16,11 +16,54 @@ function Grace() {
         <div className="header"> Grace Zhang </div>
         <div className="intro"> Hello! I'm Grace, a freshman in CS. </div>
         <div className="links">
-          <p> <a href = "mailto: gracewz2@illinois.edu" className="link">Email: gracewz2@illinois.edu</a> </p>
-          <p> <a href="tel:408-658-5025" className="link">Phone: 408-658-6025</a> </p>
-          <p> <a href="https://www.linkedin.com/in/grace-w-zhang/" className="link" target="_blank" rel="noreferrer"> LinkedIn </a> </p>
-          <p> <a href="https://www.facebook.com/drowsynights/" className="link" target="_blank" rel="noreferrer"> Facebook </a> </p>
-          <p> <a href="https://www.instagram.com/dreamsinparadise/" className="link" target="_blank" rel="noreferrer"> Instagram </a> </p>
+          <p>
+            {' '}
+            <a href="mailto: gracewz2@illinois.edu" className="link">
+              Email: gracewz2@illinois.edu
+            </a>{' '}
+          </p>
+          <p>
+            {' '}
+            <a href="tel:408-658-5025" className="link">
+              Phone: 408-658-6025
+            </a>{' '}
+          </p>
+          <p>
+            {' '}
+            <a
+              href="https://www.linkedin.com/in/grace-w-zhang/"
+              className="link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {' '}
+              LinkedIn{' '}
+            </a>{' '}
+          </p>
+          <p>
+            {' '}
+            <a
+              href="https://www.facebook.com/drowsynights/"
+              className="link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {' '}
+              Facebook{' '}
+            </a>{' '}
+          </p>
+          <p>
+            {' '}
+            <a
+              href="https://www.instagram.com/dreamsinparadise/"
+              className="link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {' '}
+              Instagram{' '}
+            </a>{' '}
+          </p>
         </div>
       </div>
       <div className="divider"></div>
@@ -32,9 +75,13 @@ function Grace() {
               {' '}
               Currently maintaining 8 boxes of cheerios in my dorm at all times{' '}
             </li>
-            <li> I wake up at 6:30 every morning (by choice~) </li>
+            <li>
+              {' '}
+              I wake up at 6:30 every morning (by choice, even during the
+              weekend~){' '}
+            </li>
             <li> 120+ games installed on my phone at the moment T_T </li>
-            <li> FAR's dining hall &gt; PAR &gt;&gt;&gt; ISR </li>
+            <li> FAR's dining hall (rip) &gt; PAR &gt;&gt;&gt; ISR </li>
           </ul>
         </div>
         <div className="right">
@@ -43,17 +90,36 @@ function Grace() {
             <li> Badminton! I'm still a beginner though </li>
             <li>
               {' '}
-              Learning <a href="https://www.youtube.com/watch?v=lg4i5xCMJpI" className="link" target="_blank" rel="noreferrer"> Studio Ghibli songs</a> on the piano (my favorite is <a href="https://youtu.be/lg4i5xCMJpI?t=152" className="link" target="_blank" rel="noreferrer"> Princess
-              Mononke's theme song</a>){' '}
+              Learning{' '}
+              <a
+                href="https://www.youtube.com/watch?v=lg4i5xCMJpI"
+                className="link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {' '}
+                Studio Ghibli songs
+              </a>{' '}
+              on the piano (my favorite is{' '}
+              <a
+                href="https://youtu.be/lg4i5xCMJpI?t=152"
+                className="link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {' '}
+                Princess Mononke's theme song
+              </a>
+              ){' '}
             </li>
             <li>
               {' '}
-              Determined to get back into reading. Just waiting for Grainger to
-              process my book requests T_T{' '}
+              Determined to get back into reading. Just started{' '}
+              <i>The Light Between Oceans</i> and it's super good so far~{' '}
             </li>
             <li>
               {' '}
-              Goal: eat at every restaurant here by the end of the school year~{' '}
+              Goal: eat at every restaurant here by the end of the school year{' '}
             </li>
           </ul>
         </div>
@@ -61,10 +127,12 @@ function Grace() {
       <div></div>
       <div className="divider"></div>
       <div className="videos">
-        <GraceVideo url = {"https://www.youtube.com/embed/ThE2Ap9RPz8"}/>
-        <GraceVideo url = {"https://www.youtube.com/embed/gLDSU36nNls"}/>
-        <GraceVideo url = {"https://www.youtube.com/embed/ZWT9K5rRmv0"}/>
-        <GraceVideo url = {"https://www.youtube.com/embed/8bR1glLPtaY?start=18"}/>
+        <GraceVideo url={'https://www.youtube.com/embed/ThE2Ap9RPz8'} />
+        <GraceVideo url={'https://www.youtube.com/embed/gLDSU36nNls'} />
+        <GraceVideo url={'https://www.youtube.com/embed/ZWT9K5rRmv0'} />
+        <GraceVideo
+          url={'https://www.youtube.com/embed/8bR1glLPtaY?start=18'}
+        />
       </div>
     </>
   );
--- client/src/pages/GraceVideo.jsx
+++ client/src/pages/GraceVideo.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
-function GraceVideo(prop) {
-    const {
-        url,
-    } = prop;
-    return (
-        <div>
-            <iframe
-                width="390"
-                height="250"
-                src={url}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            />
-        </div>
-    );
+function GraceVideo(props) {
+  const { url } = props;
+  return (
+    <div>
+      <iframe
+        width="395"
+        height="250"
+        src={url}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
 }
 
 export default GraceVideo;
