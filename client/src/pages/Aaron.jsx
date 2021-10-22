3934db7ae6ef433ed510f37c3eb173a41a249170
Aaron Alexander <a.alexander72003@gmail.com>
2021-10-22 01:03:42 -0500

    Final version

--- client/src/pages/Aaron.jsx
+++ client/src/pages/Aaron.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 import AaronNavbar from '../components/AaronNavbar';
 import AaronHeader from '../components/AaronHeader';
-import AaronP from '../components/AaronP';
 import '../css/Aaron.css';
 
 function Aaron() {
@@ -14,7 +13,14 @@ function Aaron() {
           <AaronHeader />
           <AaronNavbar />
         </div>
-        <AaronP />
+        <hr className="cool-hr"></hr>
+      <p className="cool-intro">
+        Welcome to my personal website! I am Aaron Alexander, a freshman
+        studying computer science at the University of Illinois at
+        Urbana-Champaign and a proud Academy Member at Hack4Impact. So so so
+        excited to start my journey in software development at such a great
+        school!
+      </p>
       </div>
     </>
   );
