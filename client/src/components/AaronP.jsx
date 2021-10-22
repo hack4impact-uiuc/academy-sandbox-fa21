207b4cd7a315244d8ac92eaeee84d942c05d9fab
Aaron Alexander <a.alexander72003@gmail.com>
2021-10-21 22:15:43 -0500

    Test

--- client/src/components/AaronP.jsx
+++ client/src/components/AaronP.jsx
@@ -5,13 +5,13 @@ function AaronP() {
   return (
     <>
       <hr className="cool-hr"></hr>
-        <p className="cool-intro">
-          Welcome to my personal website! I am Aaron Alexander, a freshman
-          studying computer science at the University of Illinois at
-          Urbana-Champaign and a proud Academy Member at Hack4Impact. So so so
-          excited to start my journey in software development at such a great
-          school!
-        </p>
+      <p className="cool-intro">
+        Welcome to my personal website! I am Aaron Alexander, a freshman
+        studying computer science at the University of Illinois at
+        Urbana-Champaign and a proud Academy Member at Hack4Impact. So so so
+        excited to start my journey in software development at such a great
+        school!
+      </p>
     </>
   );
 }
